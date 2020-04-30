035daf972b42d2db5c78ece5211477b202d2b50b
Mayank Zalavadia <mayankzalavadia@gmail.com>
2020-04-30 21:58:34 +0530

    Updates

---
+++ precache-manifest.222881f246b5f97d744d4236cc5fa586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea47674f7c4f0a937ba2de1fa5ec6b9",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "386d6d2f1c9789dc8785",
+    "url": "/covid19/static/css/main.c63a6bdc.chunk.css"
+  },
+  {
+    "revision": "fa12553f6ddbab8be247",
+    "url": "/covid19/static/js/2.42d02f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.42d02f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386d6d2f1c9789dc8785",
+    "url": "/covid19/static/js/main.5c217733.chunk.js"
+  },
+  {
+    "revision": "bb08661687ef9ed76af2",
+    "url": "/covid19/static/js/runtime-main.06ba182b.js"
+  }
+]);
